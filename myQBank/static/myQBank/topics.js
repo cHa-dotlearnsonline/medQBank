f6059c5b4337dd0366938c45cf5bd33ad3ace855
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2022-04-12 07:40:51 +0200

    just manipulated javascript thatdoesn't work properly. I need to write better code

--- myQBank/static/myQBank/topics.js
+++ myQBank/static/myQBank/topics.js
@@ -4,54 +4,78 @@ function allowAttempt() {
     var attempt_button = document.querySelector('#attemptButton')
     var attempt_button_id = attempt_button.dataset.id
     var courseID = parseInt(attempt_button_id)
+    var attemptedQuestionsList = []
     fetch(`http://127.0.0.1:8000/addCourse/${courseID}`)
-    .then(response => response.json())
-    .then(taken => {
-        check_course = taken['taking']
-        if (check_course === true) {
-            show_button = attempt_button.style.display = 'block'
-            attempt_button.addEventListener('click', () => {
-                if (attempt_button.innerHTML === "Attempt") {
-                    attempt_button.innerHTML = "Stop Attempt"
-                    answer_set = document.querySelectorAll(".answer")
-                    answer_set.forEach(answer => {
-                        var clickedness = answer.dataset.clicked
-                        clickedness = parseInt(clickedness)
-                        if (clickedness === 0) {
-                            answer.addEventListener('click', () => {
-                                
-                                var questionID = parseInt(answer.dataset.questionid)
-                                var questionNumber = document.querySelector(`#question${questionID}`)
-                                console.log(answer.dataset.clicked)
-                                if (parseInt(answer.dataset.clicked) === 0) {
-                                    questionNumber.dataset.attempts = parseInt(questionNumber.dataset.attempts) + 1  
-                                }
-                                answer.dataset.clicked = 1
-                                
-                                var trueness = answer.dataset.correctness
-                                var button2 = document.querySelector('#attemptButton')
-                                console.log(questionNumber.dataset.attempts)
-                                if (button2.innerHTML === "Stop Attempt" && trueness === "True") {
-                                    answer.style.color ="green"
-                                } else if (button2.innerHTML === "Stop Attempt" && trueness ==="False") {
-                                    answer.style.color = "red"
-                                }
-                            })
+        .then(response => response.json())
+        .then(taken => {
+            check_course = taken['taking']
+            if (check_course === true) {
+                show_button = attempt_button.style.display = 'block'
+                attempt_button.addEventListener('click', () => {
+                    if (attempt_button.innerHTML === "Attempt") {
+                        attempt_button.innerHTML = "Stop Attempt"
+                        answer_set = document.querySelectorAll(".answer")
+                        answer_set.forEach(answer => {
+                            var clickedness = answer.dataset.clicked
+                            clickedness = parseInt(clickedness)
+                            if (clickedness === 0) {
+                                answer.addEventListener('click', () => {
+                                    isStillAttempting = document.querySelector("#attemptButton").innerHTML
+                                    if (isStillAttempting === "Stop Attempt") {
+                                        var questionID = parseInt(answer.dataset.questionid)
+                                        var questionNumber = document.querySelector(`#question${questionID}`)
+                                        if (!attemptedQuestionsList.includes(`question${questionID}`)) {
+                                            attemptedQuestionsList.push(`question${questionID}`)
+                                        }
+                                        if (parseInt(answer.dataset.clicked) === 0) {
+                                            questionNumber.dataset.attempts = parseInt(questionNumber.dataset.attempts) + 1
+                                        }
+                                        answer.dataset.clicked = 1
+                                        var trueness = answer.dataset.correctness
+                                        var button2 = document.querySelector('#attemptButton')
+                                        if (button2.innerHTML === "Stop Attempt" && trueness === "True") {
+                                            answer.style.color = "green"
+                                            answer.dataset.correctClick = "yes"
+                                            questionNumber.dataset.correct = 1
+                                        } else if (button2.innerHTML === "Stop Attempt" && trueness === "False") {
+                                            answer.style.color = "red"
+                                        }
 
-                        }
+                                    }
+                                })
 
-                    })
+                            }
 
-                } else if (attempt_button.innerHTML === "Stop Attempt") {
-                    attempt_button.innerHTML = "Attempt"
-                }
-            })
-        } else {
-           messenger = document.querySelector('#messenger')
-           messenger.innerText = "Add the course First Before you can attempt"
-           messenger.style.color ="red"
-           messenger.style.fontWeight = "bold"
-            attempt_button.style.display = 'none'
-        }
-    })
+                        })
+
+                    } else if (attempt_button.innerHTML === "Stop Attempt") {
+                        attemptedQuestionsList.forEach(question => {
+                            var questionDetails = document.querySelector(`#${question}`)
+                            var correctAnswerClicked = questionDetails.dataset.correct
+                            if (correctAnswerClicked === undefined) {
+                                var correct = 0
+                            } else {
+                                var correct = parseInt(questionDetails.dataset.correct)
+                            }
+                            var question_id = parseInt(questionDetails.dataset.questionid)
+                            var attempts = parseInt(questionDetails.dataset.attempts)
+                            var course = questionDetails.dataset.course
+                            var topic = questionDetails.dataset.topic
+
+                            console.log(`Correct Clicks: ${correct},\nQuestion ID: ${question_id},\nTotal Attempts: ${attempts},\nCourse: ${course},\nTopic: ${topic}\n`) 
+                            console.log(attemptedQuestionsList)
+                            index = attemptedQuestionsList.indexOf(question)
+                            attempt_button.innerHTML = "Attempt"
+                        })
+                    }
+                    
+                })
+            } else {
+                messenger = document.querySelector('#messenger')
+                messenger.innerText = "Add the course First Before you can attempt"
+                messenger.style.color = "red"
+                messenger.style.fontWeight = "bold"
+                attempt_button.style.display = 'none'
+            }
+        })
 }
