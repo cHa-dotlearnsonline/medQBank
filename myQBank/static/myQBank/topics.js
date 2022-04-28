ef47e93089841a9ecb8c1008881d9d700e423223
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2022-04-28 21:10:42 +0200

    Made sure I added indicator to show the correct answer once a wrong answer is clicked

--- myQBank/static/myQBank/topics.js
+++ myQBank/static/myQBank/topics.js
@@ -5,8 +5,14 @@ function allowAttempt() {
     var attempt_button = document.querySelector('#attemptButton')
     var attempt_button_id = attempt_button.dataset.id
     var courseID = parseInt(attempt_button_id)
-    var attemptedQuestionsList = []
-    var allready_attempted = {}
+    correctSet = document.querySelectorAll('.correct-answer')
+    correctSet.forEach(indicator => {
+        indicator.style.display = 'none'
+    })
+    wrongSet = document.querySelectorAll('.wrong-answer')
+    wrongSet.forEach(wrong => {
+        wrong.style.display = 'none'
+    })
     fetch(`/addCourse/${courseID}`)
         .then(response => response.json())
         .then(taken => {
@@ -20,69 +26,65 @@ function allowAttempt() {
                         attempt_button.classList.remove("btn-secondary")
                         answer_set = document.querySelectorAll(".answer")
                         answer_set.forEach(answer => {
-                            var clickedness = answer.dataset.clicked
+                            var questionID = parseInt(answer.dataset.questionid)
+                            var questionNumber = document.querySelector(`#question${questionID}`)
+                            var clickedness = questionNumber.dataset.clicked
+                            var isStillAttempting = document.querySelector("#attemptButton").innerHTML
                             clickedness = parseInt(clickedness)
-                            if (clickedness === 0) {
+                            if (clickedness === 0 && isStillAttempting === "Stop Attempt") {
+                                console.log(questionNumber.dataset.clicked)
                                 answer.addEventListener('click', () => {
-                                    isStillAttempting = document.querySelector("#attemptButton").innerHTML
-                                    if (isStillAttempting === "Stop Attempt") {
-                                        var questionID = parseInt(answer.dataset.questionid)
-                                        var questionNumber = document.querySelector(`#question${questionID}`)
-                                        if (!attemptedQuestionsList.includes(`question${questionID}`)) {
-                                            attemptedQuestionsList.push(`question${questionID}`)
-                                        }
+                                    
+                                    if (isStillAttempting === "Stop Attempt" && parseInt(questionNumber.dataset.clicked) === 0) {
                                         if (parseInt(answer.dataset.clicked) === 0) {
                                             questionNumber.dataset.attempts = parseInt(questionNumber.dataset.attempts) + 1
                                         }
                                         answer.dataset.clicked = 1
                                         var trueness = answer.dataset.correctness
                                         var button2 = document.querySelector('#attemptButton')
+                                        var correctIndicator = document.querySelector(`#correct-answer${questionID}`)
+                                        var wrongIndicator = document.querySelector(`#wrong-answer${questionID}`)
                                         if (button2.innerHTML === "Stop Attempt" && trueness === "True") {
                                             answer.style.color = "green"
                                             answer.dataset.correctClick = "yes"
                                             questionNumber.dataset.correct = 1
+                                            correctIndicator.insertAdjacentHTML("beforeend", "CORRECT")
+                                            correctIndicator.style.display = 'block'
                                         } else if (button2.innerHTML === "Stop Attempt" && trueness === "False") {
                                             answer.style.color = "red"
+                                            questionNumber.dataset.correct = 0
+                                            wrongIndicator.insertAdjacentHTML("beforeend", "WRONG")
+                                            wrongIndicator.style.display = 'block'
+                                            all_question_answers = document.querySelectorAll(`.answer${questionID}`)
+                                            all_question_answers.forEach(option => {
+                                                truthfulness = option.dataset.correctness 
+                                                if (truthfulness === "True") {
+                                                    option.style.color = 'green'
+                                                    option.insertAdjacentHTML("beforeend", `<i class="fi fi-bs-check"></i>`)
+                                                }
+                                            })
                                         }
-
+                                        fetch('/attempts',{
+                                            method: 'POST',
+                                            body: JSON.stringify({
+                                                question:questionID,
+                                                attempts:questionNumber.dataset.attempts,
+                                                correct:questionNumber.dataset.correct,
+                                                course:questionNumber.dataset.course,
+                                                topic:questionNumber.dataset.topic,
+                                                }),
+                                                headers: { 'X-CSRFToken': csrftoken},
+                                                mode: 'same-origin'
+                                            })
                                     }
+                                    questionNumber.dataset.clicked = 1
                                 })
 
                             }
 
                         })
 
-                    } else if (attempt_button.innerHTML === "Stop Attempt") {
-                        attemptedQuestionsList.forEach(question => {
-                            var questionDetails = document.querySelector(`#${question}`)
-                            var correctAnswerClicked = questionDetails.dataset.correct
-                            if (correctAnswerClicked === undefined) {
-                                var correct = 0
-                            } else {
-                                var correct = parseInt(questionDetails.dataset.correct)
-                            }
-                            var question_id = parseInt(questionDetails.dataset.questionid)
-                            var attempts = parseInt(questionDetails.dataset.attempts)
-                            var course = questionDetails.dataset.course
-                            var topic = questionDetails.dataset.topic
-
-                            if (allready_attempted[`${question}`] === undefined || allready_attempted[`${question}`] !== attempts) {
-                                allready_attempted[`${question}`] = attempts
-                                console.log(`Correct Clicks: ${correct},\nQuestion ID: ${question_id},\nTotal Attempts: ${attempts},\nCourse: ${course},\nTopic: ${topic}\n`)
-                                fetch('/attempts', {
-                                    method: 'POST',
-                                    body: JSON.stringify({
-                                        question: question_id,
-                                        attempts: attempts,
-                                        correct: correct,
-                                        course: course,
-                                        topic: topic
-                                    }),
-                                    headers: { 'X-CSRFToken': csrftoken },
-                                    mode: 'same-origin'
-                                })
-                            }
-                        })                                                                                                                              
+                    } else if (attempt_button.innerHTML === "Stop Attempt") {                                                                                                                              
                         attempt_button.innerHTML = "Attempt"
                         attempt_button.classList.add("btn-secondary")
                         attempt_button.classList.remove("btn-success")
