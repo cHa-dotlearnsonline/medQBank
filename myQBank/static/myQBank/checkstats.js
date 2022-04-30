7df5dbcbfd4e3e79e5c8c9474b483b908e9f7af8
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2022-04-30 07:16:08 +0200

    more changes

--- myQBank/static/myQBank/checkstats.js
+++ myQBank/static/myQBank/checkstats.js
@@ -5,7 +5,16 @@ function checkStats() {
     statsButton = document.querySelectorAll(".subjectStats")
     statsButton.forEach(button => {
         button.addEventListener('click', () => {
-            course_id = parseInt(button.dataset["course"])
+            var course_id = parseInt(button.dataset["course"])
+            
+            let viewStats = document.querySelector(`#display-stats${course_id}`)
+            var show_stats = parseInt(viewStats.dataset["show"])
+            if ( show_stats > 0) {
+                viewStats.style.display = "none"
+                viewStats.dataset["show"] = 0
+            } else {
+                
+           
             fetch('/attempts', {
                 method: 'PUT',
                 body: JSON.stringify({
@@ -16,23 +25,27 @@ function checkStats() {
             })
             .then(response => response.json())
             .then(stats => {
+                var courseID = parseInt(stats["Course ID"])
+                let viewStats = document.querySelector(`#display-stats${courseID}`)
                 if (stats["Total Attempts"] !== undefined) {
                     var total_attempts = `Total Attempts: ${stats["Total Attempts"]}<br>`
                     var total_correct  = `Total Correct: ${stats["Total Correct"]}<br>`
                     var pass_percentage  = `Pass Percentage: ${stats["Pass Percentage"]}%`
                     var courseID = parseInt(stats["Course ID"])
-                    console.log(`${total_attempts}\n${total_correct}\n${pass_percentage}%`)
-                    let viewStats = document.querySelector(`#display-stats${courseID}`)
+                    console.log(`${total_attempts}\n${total_correct}\n${pass_percentage}%`) 
                     viewStats.innerHTML = total_attempts + total_correct + pass_percentage
-
+                    viewStats.style.display = "block"
+                    viewStats.dataset["show"] = 1
                 }
 
                 else if (stats["error"] !== undefined) {
                     var courseID = parseInt(stats["Course ID"])
-                    let viewStats = document.querySelector(`#display-stats${courseID}`)
                     viewStats.innerHTML = stats["error"]
+                    viewStats.style.display = "block"
+                    viewStats.dataset["show"] = 1
                 }
             })
+        } 
         })
     })
 }
