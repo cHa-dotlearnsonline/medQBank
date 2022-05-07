fe20d51d65e727d469b0ef42b2f03d47db9328d7
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2022-05-07 21:47:31 +0200

    Continued to add javascript to help with editing answers

--- myQBank/static/myQBank/editAns.js
+++ myQBank/static/myQBank/editAns.js
@@ -1,8 +1,43 @@
 Edit()
 
 function Edit() {
-    edit_views = document.querySelectorAll('.change_answer')
+    var edit_views = document.querySelectorAll('.change_answer')
     edit_views.forEach(view => {
         view.style.display = 'none'
     })
+   var edit_buttons = document.querySelectorAll('.edit')
+   edit_buttons.forEach(button => {
+       let questionID = parseInt(button.dataset['questionid'])
+       button.addEventListener('click', () => {
+           let editButtonDisplay = document.querySelector(`#edit${questionID}`)
+           editButtonDisplay.style.display = 'none'
+           let edit_boy = document.querySelector(`#change_answer${questionID}`)
+           edit_boy.style.display = 'block'
+           let card_boy = document.querySelector(`#card${questionID}`)
+           card_boy.style.display = 'none'
+           let save_boy = document.querySelector(`#savebutton${questionID}`)
+           save_boy.addEventListener('click', () => {
+               edit_boy.style.display = 'none'
+               card_boy.style.display = 'block'
+               editButtonDisplay.style.display = 'block'
+           })
+       })
+   }) 
+}
+
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break
+            }
+        }
+    }
+    return cookieValue;
 }
