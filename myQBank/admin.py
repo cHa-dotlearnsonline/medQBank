6ca95a1a59d5e5e61287de43d5aaaffe1ca227fe
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2022-09-22 19:20:33 +0200

    Made sure to add the attempt model to the admin

--- myQBank/admin.py
+++ myQBank/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Attempted, EssayAnswer, EssayQuestion, GrandAttempt, SubQuestion, User, Question, Answer, Course, Topic, Userscourses
+from .models import Attempt, Attempted, EssayAnswer, EssayQuestion, GrandAttempt, SubQuestion, User, Question, Answer, Course, Topic, Userscourses
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Question)
@@ -8,6 +8,7 @@ admin.site.register(Answer)
 admin.site.register(Course)
 admin.site.register(Topic)
 admin.site.register(Attempted)
+admin.site.register(Attempt)
 admin.site.register(GrandAttempt)
 admin.site.register(EssayQuestion)
 admin.site.register(EssayAnswer)
