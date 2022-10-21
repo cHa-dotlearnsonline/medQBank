a0da8e65c3e928f8194b2c6f1437374170ebc012
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2022-10-21 20:07:32 +0200

    made sure that debug is set to false so that no effor information is displayed to the user

--- qbank/settings.py
+++ qbank/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pi-emhbua8_+m#d#eeyra6fg!14o(fozu48kh+cjmz(xjac9n-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','medqbank.herokuapp.com']
 
