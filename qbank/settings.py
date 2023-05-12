6da5c337c829e63a815c82164640c49d65d3be3a
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2023-05-12 14:52:07 +0200

    All new changes

--- qbank/settings.py
+++ qbank/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pi-emhbua8_+m#d#eeyra6fg!14o(fozu48kh+cjmz(xjac9n-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','medqbank.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','medqbank.herokuapp.com', '*']
 
 
 # Application definition
